cd4f9a92e02ceed0f7664422f582d0b3f78bb704
yiyaang <yiyaaang@gmail.com>
2025-02-28 00:07:45 +0900

    환경변수 출력로그 제거

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-// 디버깅을 위한 환경 변수 출력
-console.log('SUPABASE URL:', import.meta.env.VITE_SUPABASE_URL);
-console.log('SUPABASE KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
+// // 디버깅을 위한 환경 변수 출력
+// console.log('SUPABASE URL:', import.meta.env.VITE_SUPABASE_URL);
+// console.log('SUPABASE KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
